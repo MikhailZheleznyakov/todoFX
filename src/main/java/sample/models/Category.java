9127d75c49883df67706e3d3fe764e697f653848
MikhailZheleznyakov <ironguy.cool@yandex.ru>
2021-05-26 22:01:49 +0300

    update controllers

--- src/main/java/sample/models/Category.java
+++ src/main/java/sample/models/Category.java
@@ -1,4 +1,68 @@
 package sample.models;
 
-public class Category {
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.beans.property.*;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
+
+public class Category implements APIModel{
+    private final StringProperty name;
+    private final LongProperty id;
+
+    public Category(Long id, String name, Task task){
+        this.id = new SimpleLongProperty(id);
+        this.name = new SimpleStringProperty(name);
+    }
+//    ObjectMapper mapper = new ObjectMapper();
+//
+//    try {
+//        return mapper.writeValueAsString(this);
+//    }
+//    catch (JsonGenerationException | JsonMappingException e) {
+//        e.printStackTrace();
+//    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject map = new JSONObject();
+        map.put("id", id.get());
+        map.put("name", name.get());
+        return map;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id.set(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name=" + name +
+                ", id=" + id +
+                '}';
+    }
 }
--- src/main/java/sample/models/Task.java
+++ src/main/java/sample/models/Task.java
@@ -1,4 +1,101 @@
 package sample.models;
 
-public class Task {
+import javafx.beans.property.*;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
+
+public class Task implements APIModel{
+    private final StringProperty name;
+    private final StringProperty description;
+    private final ObjectProperty<LocalDate> deadline;
+    private final LongProperty id;
+    private User user;
+
+    public Task(Long id, String name, String description, Object deadline, User user) {
+        this.id = new SimpleLongProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.description = new SimpleStringProperty(description);
+        this.deadline = new SimpleObjectProperty<LocalDate>((LocalDate) deadline);
+        this.user = user;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject map = new JSONObject();
+        map.put("id", id.get());
+        map.put("name", name.get());
+        map.put("description", description.get());
+        map.put("deadline", deadline.get());
+        map.put("user", user.toJson());
+
+        return map;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description.set(description);
+    }
+
+    public LocalDate getDeadline() {
+        return deadline.get();
+    }
+
+    public ObjectProperty<LocalDate> deadlineProperty() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline.set(deadline);
+    }
+
+    public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id.set(id);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name=" + name +
+                ", description=" + description +
+                ", deadline=" + deadline +
+                ", id=" + id +
+                ", user=" + user +
+                '}';
+    }
 }
